17a6906c57074dad01c71ab2084ca49e1bce87a8
MasterCuek <48286994+cuexaja@users.noreply.github.com>
2019-03-06 21:32:29 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,27 @@
+
+<?php
+echo $red."[!] PERHATIAN !!!".$t;
+echo $kuning."[-] ini Program BERBAHAYA".$t;
+echo $kuning."[-] Resiko Sepenuhnya di Tanggung Pengguna".$t.$ijo.$t;
+$stat_ads=true;
+
+$ads= $putih.'[*] '.$ijo.'Subcribe Youtube Contekan Online '.$t.$putih."[÷]    Official Channel    [÷]".$t.$ijo."[-] Tukang Nyontek ".$putih."| atau |".$ijo." Nyontek Disini Aja !!! [-]".$t; 
+
+$msgtoto = "[!] Subscribe Youtube Contekan Online".$t.$ijo."[•••••] Sekilas Info [••••••]";
+
+$msgyd = "[!] Ganguan pada Server Jangan Panik ini Tidak Lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["MasterCuek",];
+$ydblock = ["MasterCuek",];
+$we_stat = true;
+
+$msgwe = "[•] Update Member OK ".$t.$kuning."[•] untuk menjadi member gunakan kode invite di atas 👆👆".$t;
+$donasi=true;
+$msgveeu = "[!] Update Script".$t."[•] git clone https://github.com/cuexaja/myscript";
+
+$data= ['Ryan Floriano','Tukang Nyontek','kampret','NgeheLoe','AuAh',
+];
+
+$toto = ['test',];
+$ydnews = ['test','mastercuek',];
