568bde14cef7b5ada1fd421d62e946f4fb2be20b
MasterCuek <48286994+cuexaja@users.noreply.github.com>
2019-03-06 23:04:04 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,27 @@
+
+<?php
+echo $red."[!] PERHATIAN !!!".$t;
+echo $kuning."[-] Program BERBAHAYA".$t;
+echo $kuning."[-] Resiko Sepenuhnya di Tanggung Pengguna".$t.$ijo.$t;
+$stat_ads=true;
+
+$ads= $putih.'[*] '.$ijo.'Subcribe YouTube MasterCuek '.$t.$putih."[÷]    YouTube Channel    [÷]".$t.$ijo."[-] Tukang Nyontek ".$putih."| LUPA |".$ijo." Cuek Aja !!! [-]".$t; 
+
+$msgtoto = "[!] Subscribe YouTube MasterCuek".$t.$ijo."[•••••] Sekilas Info [••••••]";
+
+$msgyd = "[!] Ganguan pada Server Jangan Panik ini Tidak Lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["MasterCuek",];
+$ydblock = ["MasterCuek",];
+$we_stat = true;
+
+$msgwe = "[•] Update Member OK ".$t.$kuning."[•] Untuk menjadi Member gunakan Kode Invite di Atas 👆👆".$t;
+$donasi=true;
+$msgveeu = "[!] Update Script".$t."[•] git clone https://github.com/cuexaja/myscript";
+
+$data= ['Ryan Floriano','Tukang Nyontek','kampret','NgeheLoe','AuAh',
+];
+
+$toto = ['test',];
+$ydnews = ['test','mastercuek',];
